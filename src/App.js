9d9ff81152f1ad2898eb0f993d20e2e469a531eb
Lizia Towata <liziatowata@gmail.com>
2022-10-07 17:09:08 -0300

    requisito 03

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Game from './pages/Game';
+import Settings from './pages/Settings';
 // import logo from './trivia.png';
 import './App.css';
 
@@ -11,6 +12,7 @@ class App extends Component {
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route path="/games" component={ Game } />
+        <Route path="/configuracoes" component={ Settings } />
       </Switch>
     );
   }
