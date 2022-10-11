e71835860ca3f1df411a3cce1a709a29ccfa0585
Robson Silva <rm8l@outlook.com>
2022-10-11 16:50:14 -0300

    test

--- src/tests/Feedbacks.test.js
+++ src/tests/Feedbacks.test.js
@@ -1,15 +1,28 @@
 import React from 'react';
-// import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+ import { screen } from '@testing-library/react';
+ import userEvent from '@testing-library/user-event';
 import { renderWithRouterAndRedux } from './helpers/renderWithRouterAndRedux';
-// import App from '../App';
-import Feedback from '../pages/Feedback';
+import App from '../App'
+import Feedback from '../pages/Feedback'
+
+
 
 describe('Desenvolva testes para atingir 90% de cobertura da tela de Feedbacks', () => {
   test('a rota esta página seria /feedback', () => {
-    const { history } = renderWithRouterAndRedux(<Feedback />);
+    const initialState = {
+      state:'test'
+    };
+
+   const { history } = renderWithRouterAndRedux(<Feedback />, initialState, '/feedback');
     const { location: { pathname } } = history;
     expect(pathname).toEqual('/feedback');
+
+    const points = screen.getByText(/pontos:/i)
+    expect(points).toBeInTheDocument();
+    const playAgain = screen.getByRole('button', { name: /play again/i });
+    expect(playAgain).toBeInTheDocument();
+    userEvent.click(playAgain);
+    expect(pathname).toEqual('/');
   });
 
   test('', () => {
